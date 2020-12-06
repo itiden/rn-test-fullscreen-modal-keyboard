98fd2d40b86de6c96ffc666a81c96e65b2413db3
Andreas Bergqvist <andreas@itiden.se>
2020-12-06 20:17:18 +0100

    Fix app back to active immersive

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { observer } from 'mobx-react';
 import * as React from 'react';
+import { AppState, AppStateStatus } from 'react-native';
 import { StatusBar } from 'react-native';
+import { immersiveModeOn } from 'react-native-android-immersive-mode';
 import changeNavigationBarColor from 'react-native-navigation-bar-color';
 import { Host } from 'react-native-portalize';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -17,7 +19,16 @@ const fullscreenStore = createFullscreenStore();
 
 const App = observer(() => {
   React.useEffect(() => {
+    function handleAppStateChange(state: AppStateStatus) {
+      if (state === 'active' && fullscreenStore.fullscreen) {
+        immersiveModeOn();
+      }
+    }
     changeNavigationBarColor('black', true, false);
+    AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
   }, []);
   return (
     <AppProvider
